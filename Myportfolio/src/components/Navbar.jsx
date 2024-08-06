b3e7587e24659a94cb71a70f335849088ae2879c
JassiJassika <jassika.jassika.jassika@gmail.com>
2024-08-06 23:22:18 +0530

    commit

--- Myportfolio/src/components/Navbar.jsx
+++ Myportfolio/src/components/Navbar.jsx
@@ -8,25 +8,24 @@ function Navbar() {
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">
-            <img src ={''} alt="logo">
-            </img></Navbar.Brand>
+          <img src={''} alt="logo" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#Projects">Projects</Nav.Link>
           </Nav>
+          <div className='navbar-text'>
+            <div className='social-icon'>
+              <a href="#LinkedIn"><img src={''} alt="LinkedIn" /></a>
+              <a href="#GitHub"><img src={''} alt="GitHub" /></a>
+              <a href="#Leetcode"><img src={''} alt="Leetcode" /></a>
+              <a href="#CodingNinja"><img src={''} alt="CodingNinja" /></a>
+            </div>
+            <button className='vvd' onClick={() => console.log("connect")}>Connect</button>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
